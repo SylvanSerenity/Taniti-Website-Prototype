77190c497bebfa3e7d372fc59c7ba07507fcec31
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-16 14:16:20 +0300

    Create Footer.svelte

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,25 @@
+<footer>
+	&#169;2024 Taniti Tourism Co.
+	<a href="/contact">Contact</a>
+</footer>
+
+<style>
+	footer {
+		width: 100%;
+		margin-top: 10px;
+		padding: 20px;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		color: white;
+		background-color: seagreen;
+		border-top: 1px solid white;
+	}
+	footer a {
+		color: white;
+		text-decoration: none;
+	}
+</style>
