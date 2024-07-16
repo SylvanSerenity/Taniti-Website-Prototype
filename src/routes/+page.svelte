13212348789ea14cfbe1271afba7055828a7fedd
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-16 11:10:58 +0300

    Added home page video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,22 @@
 <Nav/>
 
 <main>
+	<video autoplay muted loop playsinline>
+		<source src="/taniti.mp4" type="video/mp4">
+		Your browser does not support the video tag.
+	</video>
 </main>
+
+<style>
+	video {
+		position: absolute;
+		z-index: -1;
+		top: 0;
+		width: 100%;
+		height: 100%;
+
+		overflow: hidden;
+		object-fit: cover;
+	}
+</style>
+
