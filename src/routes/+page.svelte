60ce371ec3988bebd26ab63a5e887a5d6a321c3e
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-16 14:28:24 +0300

    Added video overlay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,9 @@
 		<source src="/taniti.mp4" type="video/mp4">
 		Your browser does not support the video tag.
 	</video>
+	<div class="video-overlay">
+		<h1><a href="/about">About</a></h1>
+	</div>
 </main>
 
 <style>
@@ -22,5 +25,25 @@
 		overflow: hidden;
 		object-fit: cover;
 	}
-</style>
 
+	.video-overlay {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		padding: 15px;
+
+		background-color: #00800025;
+		font-size: 20px;
+		border-radius: 16px;
+		transform: translate(-50%, -50%) scale(1.0);
+		transition: transform 0.3s ease, background-color 0.3s ease;
+	}
+	.video-overlay a {
+		color: white;
+		text-decoration: none;
+	}
+	.video-overlay:hover {
+		background-color: #00800055;
+		transform: translate(-50%, -50%) scale(1.1);
+	}
+</style>
