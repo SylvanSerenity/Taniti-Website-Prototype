93691834a31d55f85463003ffda62faf426496e9
Avery Aaron <sylvanserene@gmail.com>
2024-07-17 19:01:11 +0300

    Added map page

---
+++ src/routes/map/page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Nav from '../Nav.svelte';
+	import Footer from '../Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Map of Taniti</title>
+</svelte:head>
+
+<Nav />
+
+<main>
+	<!-- TODO -->
+</main>
+
+<Footer />
