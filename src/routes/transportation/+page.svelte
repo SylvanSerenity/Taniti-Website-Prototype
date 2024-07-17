6b434ccca1787162b86c2794833e4e7d8bec7f9f
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 11:33:46 +0300

    Create transportation/+page.svelte

---
+++ src/routes/transportation/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Nav from '../Nav.svelte';
+	import Footer from '../Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Transportation</title>
+</svelte:head>
+
+<Nav />
+
+<main>
+	<div class="split-content">
+		<div class="text-container">
+			<h2>Arrival & Departure</h2>
+			<p>You can arrive by plane!</p>
+			<p>You can depart by cruise!</p>
+		</div>
+		<div class="text-container">
+			<h2>Ground Transportation</h2>
+			<p>You can travel by car!</p>
+		</div>
+	</div>
+</main>
+
+<Footer />
