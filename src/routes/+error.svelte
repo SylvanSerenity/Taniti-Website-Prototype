8ac4d416ee1a4b940dfe09d9a701ce71a51cc592
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 11:34:51 +0300

    Removed unused content class

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,22 +11,14 @@
 <Nav />
 
 <main>
-	<div class="content">
-		<h1>Something went wrong...</h1>
-		<p>{$page.status ? $page.status + ': ' : ''}{$page.error? $page.error.message : 'An unknown error occurred.'}</p>
-		<a href="/">Home</a>
-	</div>
+	<h1>Something went wrong...</h1>
+	<p>{$page.status ? $page.status + ': ' : ''}{$page.error? $page.error.message : 'An unknown error occurred.'}</p>
+	<a href="/">Home</a>
 </main>
 
 <Footer />
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 	a {
 		text-align: center;
 		background-color: #003b6d;
