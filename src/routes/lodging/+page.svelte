dc1b9f4b89041620a4e18f717c0de0cd46537743
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 13:17:00 +0300

    Create lodging/+page.svelte

---
+++ src/routes/lodging/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Nav from '../Nav.svelte';
+	import Footer from '../Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Lodging</title>
+</svelte:head>
+
+<Nav />
+
+<main>
+	<h1>Lodging</h1>
+	<div class="split-content">
+		<div class="showcase-container">
+			<img src="/taniti-resort.jpg" alt="Taniti Resort">
+			<h2>Taniti Resort</h2>
+			<p>Resort</p>
+			<p>****</p>
+			<p>$799/night</p>
+		</div>
+		<div class="showcase-container">
+			<img src="/palm-inn.jpg" alt="Palm Inn">
+			<h2>Palm Inn</h2>
+			<p>Hotel</p>
+			<p>***</p>
+			<p>$529/night</p>
+		</div>
+		<div class="showcase-container">
+			<img src="/shawman-hotel.jpg alt="Shawman Hotel">
+			<h2>Shawman Hotel</h2>
+			<p>Hotel</p>
+			<p>***</p>
+			<p>$449/night</p>
+		</div>
+		<div class="showcase-container">
+			<img src="rijas-hostel.jpg" alt="Rijas' Hostel">
+			<h2>Rijas' Hostel</h2>
+			<p>Hostel</p>
+			<p>*</p>
+			<p>$179/night</p>
+		</div>
+		<div class="showcase-container">
+			<img src="culpan-home.jpg" alt="Culpan Family Home">
+			<h2>Culpan Family Home</h2>
+			<p>Hostel</p>
+			<p>*</p>
+			<p>$159/night</p>
+		</div>
+	</div>
+</main>
+
+<Footer />
