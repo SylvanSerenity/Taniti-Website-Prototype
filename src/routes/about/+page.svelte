085d508e69f895b712a1b968af10c03c41cde986
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 09:48:07 +0300

    Create about/+page.svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Nav from '../Nav.svelte';
+	import Footer from '../Footer.svelte';
+	import Slideshow from './Slideshow.svelte';
+
+	const slides = [
+		{src: '/slideshow/volcano.jpg', text: 'Visit our active volcano!'}
+	];
+</script>
+
+<svelte:head>
+	<title>About Taniti</title>
+</svelte:head>
+
+<main>
+	<h1>About</h1>
+	<Slideshow {slides} />
+</main>
