ac9d51559cdf30fc8243f738207f9cff7f2533e6
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 15:28:20 +0300

    Create contact/+page.svelte

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Nav from '../Nav.svelte';
+	import Footer from '../Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<Nav />
+
+<main>
+	<h1>Contact Us</h1>
+	<form>
+		<label>
+			Email:
+			<input type="text" placeholder="email@domain.ext" required />
+		</label>
+		<label>
+			Subject:
+			<input type="text" placeholder="Subject" required />
+		</label>
+		<label>
+			Message:
+			<textarea placeholder="Message..." required></textarea>
+		</label>
+		<button type="submit">Send</button>
+	</form>
+</main>
+
+<Footer />
