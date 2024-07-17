00cbe87f51e0b7ca82030aa9bb936a020c399b36
Avery Aaron <147945121+SylvanSerenity@users.noreply.github.com>
2024-07-17 13:20:47 +0300

    Fixed images

--- src/routes/lodging/+page.svelte
+++ src/routes/lodging/+page.svelte
@@ -13,35 +13,35 @@
 	<h1>Lodging</h1>
 	<div class="split-content">
 		<div class="showcase-container">
-			<img src="/taniti-resort.jpg" alt="Taniti Resort">
+			<img src="/lodging/taniti-resort.jpg" alt="Taniti Resort">
 			<h2>Taniti Resort</h2>
 			<p>Resort</p>
 			<p>****</p>
 			<p>$799/night</p>
 		</div>
 		<div class="showcase-container">
-			<img src="/palm-inn.jpg" alt="Palm Inn">
+			<img src="/lodging/palm-inn.jpg" alt="Palm Inn">
 			<h2>Palm Inn</h2>
 			<p>Hotel</p>
 			<p>***</p>
 			<p>$529/night</p>
 		</div>
 		<div class="showcase-container">
-			<img src="/shawman-hotel.jpg alt="Shawman Hotel">
+			<img src="/lodging/shawman-hotel.jpg alt="Shawman Hotel">
 			<h2>Shawman Hotel</h2>
 			<p>Hotel</p>
 			<p>***</p>
 			<p>$449/night</p>
 		</div>
 		<div class="showcase-container">
-			<img src="rijas-hostel.jpg" alt="Rijas' Hostel">
+			<img src="/lodging/rijas-hostel.jpg" alt="Rijas' Hostel">
 			<h2>Rijas' Hostel</h2>
 			<p>Hostel</p>
 			<p>*</p>
 			<p>$179/night</p>
 		</div>
 		<div class="showcase-container">
-			<img src="culpan-home.jpg" alt="Culpan Family Home">
+			<img src="/lodging/culpan-home.jpg" alt="Culpan Family Home">
 			<h2>Culpan Family Home</h2>
 			<p>Hostel</p>
 			<p>*</p>
